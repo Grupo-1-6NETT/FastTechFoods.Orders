a2ed983622bb91bbe575e48dcd35fd0aaf6962dd
b01tech <bruno01.tech@gmail.com>
2025-07-18 13:57:54 -0300

    feat: cria migracao adiciona coluna disponibilidade e forma de pagamento

---
+++ src/Orders.Infrastructure/Migrations/20250718165649_AdicionaColunaDisponibilibade.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orders.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaColunaDisponibilibade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Disponibilidade",
+                table: "ProdutosCatalogo",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FormaDeEntrega",
+                table: "Pedidos",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disponibilidade",
+                table: "ProdutosCatalogo");
+
+            migrationBuilder.DropColumn(
+                name: "FormaDeEntrega",
+                table: "Pedidos");
+        }
+    }
+}
